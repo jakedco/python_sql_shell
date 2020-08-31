05e75fde9dbc86e08852b84cdf4ab825e2bcbdd0
jakedco <46946591+jakedco@users.noreply.github.com>
2020-08-31 10:40:50 -0400

    Initial Code

---
+++ python_sql_shell.py
@@ -0,0 +1,22 @@
+import os, pyodbc
+
+# Database details
+server = r"SERVER_NAME"
+database = "DATABASE_NAME"
+
+# Command to connect to the Database and get a cursor
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
+cursor = cnxn.cursor()
+cursor_input = ""
+
+print("enter 'q!' to quit")
+
+while cursor_input != "q!":
+    cursor_input = input('$')
+    if len(cursor_input) == 0:
+        continue
+    if cursor_input == "q!":
+        break
+    cursor.execute(cursor_input)
+    print(cursor.fetchall())
+    
